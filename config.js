934335ec2cd43bcf7ace42b9dbf55e8dbcc13266
JORGELBURGOS <jorgeleonburgos@gmail.com>
2025-08-13 11:55:51 -0300

    Delete config.js

--- config.js
+++
@@ -1,4 +0,0 @@
-// Config rápida no-CORS (Apps Script Web App)
-window.CONFIG = {
-  ENDPOINT_URL: "https://script.google.com/macros/s/AKfycbwAtS6QC068HLCYYd5aUAJSf6CE_A25HoAc4NMtFfdwwCumRukycwVETaGIqAAtuMAu/exec"
-};
