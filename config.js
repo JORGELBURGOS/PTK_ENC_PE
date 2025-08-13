3ee45ffa34443543beb8ed2821313a8efc1f40ba
JORGELBURGOS <jorgeleonburgos@gmail.com>
2025-08-13 11:56:49 -0300

    cuarta carga

---
+++ config.js
@@ -0,0 +1,4 @@
+// Config rápida no-CORS (Apps Script Web App)
+window.CONFIG = {
+  ENDPOINT_URL: "https://script.google.com/macros/s/AKfycbwQr-VPNH9xdp3NFDW87tfn3DMiCxGN4fg7Z7EcRd8AhZufdNJKmXydi0xbRmWGe9ve/exec"
+};
